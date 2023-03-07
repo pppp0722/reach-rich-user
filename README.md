1eab3a24b387ffe56c2bf1edcbc016c56ae0447f
ilhwan <poj0722@naver.com>
2023-03-07 22:47:56 +0900

    :tada: 프로젝트 생성

---
+++ README.md
@@ -0,0 +1,1 @@
+### reach-rich-user
---
+++ src/main/java/com/reachrich/reachrichuser/ReachRichUserApplication.java
@@ -0,0 +1,13 @@
+package com.reachrich.reachrichuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReachRichUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReachRichUserApplication.class, args);
+    }
+
+}
