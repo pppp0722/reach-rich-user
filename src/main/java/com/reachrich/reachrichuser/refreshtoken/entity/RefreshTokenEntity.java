b9e315551f82dcd325248ad069a53513ebc3e4d6
ilhwan <poj0722@naver.com>
2023-03-30 19:22:21 +0900

    :recycle: Refresh Token Redis 만료 기간 설정

--- src/main/java/com/reachrich/reachrichuser/refreshtoken/entity/RefreshTokenEntity.java
+++ src/main/java/com/reachrich/reachrichuser/refreshtoken/entity/RefreshTokenEntity.java
@@ -1,12 +1,14 @@
 package com.reachrich.reachrichuser.refreshtoken.entity;
 
+import static com.reachrich.reachrichuser.global.util.Const.REFRESH_TOKEN_EXPIRY_SECONDS;
+
 import java.io.Serializable;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
-@RedisHash("refreshToken")
+@RedisHash(value = "refreshToken", timeToLive = REFRESH_TOKEN_EXPIRY_SECONDS)
 @Getter
 public class RefreshTokenEntity implements Serializable {
 
